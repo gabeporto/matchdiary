2766a6b1850228c07bda548efb02cb529f3afd32
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-15 14:58:04 -0300

    feat: add TeamRepository

---
+++ server/src/main/java/gabeporto/matchdiary/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package gabeporto.matchdiary.repository;
+
+import gabeporto.matchdiary.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
