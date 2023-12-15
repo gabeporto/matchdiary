aa8b238bc6912d4bce60b04c585945c30792b1ca
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-15 15:59:45 -0300

    feat: implements services on match controller

--- server/src/main/java/gabeporto/matchdiary/controller/MatchController.java
+++ server/src/main/java/gabeporto/matchdiary/controller/MatchController.java
@@ -1,34 +1,47 @@
 package gabeporto.matchdiary.controller;
 
+import gabeporto.matchdiary.dto.match.MatchRequestDTO;
+import gabeporto.matchdiary.dto.match.MatchResponseDTO;
+import gabeporto.matchdiary.dto.team.TeamResponseDTO;
+import gabeporto.matchdiary.model.Match;
 import gabeporto.matchdiary.model.Team;
+import gabeporto.matchdiary.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/match")
 public class MatchController {
 
+    @Autowired
+    private MatchService matchService;
+
     @GetMapping("/all")
-    public String getAllMatches() {
-        return "";
+    public List<Match> getAllMatches() {
+        return matchService.getAllMatches();
     }
 
     @GetMapping("{id}")
-    public String getMatchById(@PathVariable("id") Long id) {
-        return "";
+    public ResponseEntity<MatchResponseDTO> getMatchById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(matchService.getMatchById(id));
     }
 
     @PostMapping
-    public String addMatch(@RequestBody Team team) {
-        return "";
+    public ResponseEntity<Match> addMatch(@RequestBody MatchRequestDTO matchRequestDTO) {
+        return ResponseEntity.ok(matchService.registerMatch(matchRequestDTO));
     }
 
     @PutMapping
-    public String updateMatch(@RequestBody Team team) {
-        return "";
+    public ResponseEntity<Match> updateMatch(@RequestBody MatchRequestDTO matchRequestDTO) {
+        return ResponseEntity.ok(matchService.updateMatch(matchRequestDTO));
     }
 
     @DeleteMapping("{id}")
-    public String deleteMatch(@PathVariable("id") Long id) {
-        return "";
+    public ResponseEntity<String> deleteMatch(@PathVariable("id") Long id) {
+        matchService.deleteMatch(id);
+        return ResponseEntity.ok("Partida deletada com sucesso.");
     }
 }
