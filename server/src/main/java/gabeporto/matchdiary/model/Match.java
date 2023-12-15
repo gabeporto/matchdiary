62ba6b0c59bbeb68c7c0fd5b39c9cc1401d15f54
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-15 14:17:14 -0300

    feat: add models

---
+++ server/src/main/java/gabeporto/matchdiary/model/Match.java
@@ -0,0 +1,43 @@
+package gabeporto.matchdiary.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Table(name = "match")
+@Entity(name = "match")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "date")
+    private Date date;
+    @Column(name = "score_team_one")
+    private Integer scoreTeamOne;
+    @Column(name = "score_team_two")
+    private Integer scoreTeamTwo;
+
+    @ManyToOne
+    @JsonManagedReference
+    @JoinColumn(name = "team_one_id")
+    private Team teamOne;
+
+    @ManyToOne
+    @JsonManagedReference
+    @JoinColumn(name = "team_two_id")
+    private Team teamTwo;
+
+    @ManyToOne
+    @JsonManagedReference
+    @JoinColumn(name = "team_supported_id")
+    private Team supportedTeam;
+}
