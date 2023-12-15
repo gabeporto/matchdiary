b9b7c080f7024ff19caead12992c2c7839a72b75
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-15 15:09:56 -0300

    feat: add TeamRequestDTO

---
+++ server/src/main/java/gabeporto/matchdiary/dto/team/TeamRequestDTO.java
@@ -0,0 +1,17 @@
+package gabeporto.matchdiary.dto.team;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamRequestDTO {
+    private Long id;
+    private String name;
+    private String state;
+    private String photoUrl;
+
+}
