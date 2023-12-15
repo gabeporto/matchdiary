74d3b0eda3750146054747710a5174f620332fd5
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-15 14:00:28 -0300

    feat: add backend inicial structure

---
+++ server/src/main/java/gabeporto/matchdiary/MatchdiaryApplication.java
@@ -0,0 +1,13 @@
+package gabeporto.matchdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchdiaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/gabeporto/matchdiary/MatchdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package gabeporto.matchdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
