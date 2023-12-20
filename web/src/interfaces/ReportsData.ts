a7c70d6bb8cdb140ed32c3433905c642dc54e3d5
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-20 11:16:59 -0300

    feat: add interfaces

---
+++ web/src/interfaces/ReportsData.ts
@@ -0,0 +1,9 @@
+import { TeamData } from "./TeamData";
+
+export interface ReportsData {
+    matchesQuantity: number;
+    winsQuantity: number;
+    winPercentage: number | string;
+    mostWatchedTeam: TeamData;
+    daysWithoutWatching: number;
+}
