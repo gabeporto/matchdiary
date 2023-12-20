593d9b6da3ba961ebc5767d3057bd479ea5ebae5
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-20 11:47:54 -0300

    feat: add MatchTable component

--- web/src/pages/match/MatchListPage.tsx
+++ web/src/pages/match/MatchListPage.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import Container from "../../components/Container";
 import Navbar from "../../components/Navbar";
 import Title from "../../components/Title";
+import { MatchData } from "../../interfaces/MatchData";
+import api from "../../services/api";
+import MatchTable from "../../components/MatchTable";
 
 export default function MatchListPage() {
+
+    const [data, setData] = useState<MatchData[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.getMatchData();
+                setData(response.data);
+            } catch (error) {
+                console.log('Erro ao buscar dados dos times:', error);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <>
             <Navbar />
 
             <Container>
                 <Title title="Partidas registradas"/>
+
+                <MatchTable matches={data} /> 
             </Container>
         </>
     )
--- web/src/pages/team/TeamListPage.tsx
+++ web/src/pages/team/TeamListPage.tsx
@@ -22,7 +22,6 @@ export default function TeamListPage() {
         fetchData();
     }, []);
 
-
     return (
         <>
             <Navbar />
