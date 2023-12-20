7a89168c2c9c36f407e07b76ad4f27815bd6d98d
Gabriel Porto <7gabrielporto@gmail.com>
2023-12-20 10:04:40 -0300

    feat: add Title component

---
+++ web/src/components/Title.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+const PageTitle = styled.h1`
+    font-weight: bold;
+    font-size: 35px;
+`
+
+const TitleBorder = styled.div`
+    box-shadow: 0px 3px 6px #00000029;
+    border: 3px solid #1A1A1A;
+    width: 307px;
+    margin-top: -10px;
+    margin-bottom: 50px;
+
+    @media only screen and (max-width: 338px) {
+        width: 90%;
+    }
+`
+
+interface TitleProps {
+    title: string;
+}
+
+export default function Title(props : TitleProps) {
+    return (
+        <>
+            <PageTitle>{props.title}</PageTitle>
+            <TitleBorder />
+        </>
+    )
+}
